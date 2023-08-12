b589b84eecf69e68d81cb0de8d6ce744f3f85e03
Kenniku Tolato <kenniku_you@ymail.com>
2023-08-12 07:23:10 +0000

    Initial commit for page fetcher.

---
+++ fetcher.js
@@ -0,0 +1,24 @@
+const [url, loc] = process.argv.slice(2);
+const paths = { url, loc };
+const fs = require("fs");
+const request = require("request");
+
+request(paths.url, (error, response, body) => {
+  if (error) {
+    console.log("Error", error);
+  }
+
+  if (!error && response.statusCode === 200) {
+    fs.writeFile(paths.loc, body, err => {
+      if (err) {
+        console.error(err);
+      }
+
+      fs.stat(paths.loc, (err, stats) => {
+        console.log(`Downloaded and saved ${stats.size} to ${paths.loc}` );
+      })
+      
+      
+    });
+  }
+});
